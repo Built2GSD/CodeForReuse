fa436e538001d4e807f02eb991a7a613c491794b
Ken Wagner <ken_xwagner@hotmail.com>
2017-07-24 22:50:28 -0700

    fixed
    syntax error fixed

--- WebServer.js
+++ WebServer.js
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<header>
 	<meta charset="UTF-8">
 	<title>Welcome to Sam the Server</title>
-	</head>
+	</header>
 	<body>
 	Hello you node type person or thing. <br>
 	This is what you call a static page
-	<iframe src="https:ilovecoding.org" frameborder="0">
+	<iframe src="https://google.com" frameborder="0">
 	</iframe>
 	</body>
 	</html>
