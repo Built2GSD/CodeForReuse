a8b2a7a35b98626a912cefcf77295857af266a5e
Ken Wagner <ken_xwagner@hotmail.com>
2017-07-24 22:48:59 -0700

    updated

---
+++ WebServer.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to Sam the Server</title>
+	</head>
+	<body>
+	Hello you node type person or thing. <br>
+	This is what you call a static page
+	<iframe src="https:ilovecoding.org" frameborder="0">
+	</iframe>
+	</body>
+	</html>
