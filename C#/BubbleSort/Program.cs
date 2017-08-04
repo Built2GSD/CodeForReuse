355dec4a3eb047ab6e77a2ec5b4b8f076cd9d048
Ken Wagner <ken_xwagner@hotmail.com>
2017-08-03 23:34:41 -0700

    More sorting styles

---
+++ C#/BubbleSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] givenData = { 150, 25, 42, 5, 13, 95, 78, 115 };
+            bool flag = true;
+            int temp;
+            int numLength = givenData.Length;
+
+
+        }
+    }
+}
