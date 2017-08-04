b338a02d8f799931320ce436f9282a7f748c66ba
Ken Wagner <ken_xwagner@hotmail.com>
2017-08-03 23:59:13 -0700

    finished and ran it.

--- C#/BubbleSort/Program.cs
+++ C#/BubbleSort/Program.cs
@@ -15,6 +15,25 @@ namespace BubbleSort
             int temp;
             int numLength = givenData.Length;
 
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (givenData[j + 1] > givenData[j])
+                    {
+                        temp = givenData[j];
+                        givenData[j] = givenData[j + 1];
+                        givenData[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+            foreach(int num in givenData)
+            {
+                Console.Write("\t{0}", num);
+            }
+            Console.Read();
 
         }
     }
